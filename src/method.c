39fa9bd5ef8b890e85459934905f58666a1bd5fd
Friedrich Weber <fred@reichbier.de>
2009-05-04 18:36:45 +0200

    added method argument passing

--- src/method.c
+++ src/method.c
@@ -30,8 +30,14 @@ SkObject *sk_method_create_proto(SkVM *vm) {
 
 SkObject *sk_method__clone(SkObject *slot, SkObject *self, SkObject *msg) {
     sk_message_check_argcount(msg, "Method clone", 1);
+    ArgCount i, argcount = sk_message_argcount(msg);
     SkObject *cloned = sk_object_clone(self);
-    sk_method_set_message(cloned, sk_message_arg_at(msg, 0));
+    SkObject *argument_names = sk_list_create(SK_VM);
+    for(i = 0; i < argcount - 1; i++) {
+        sk_list_append(argument_names, sk_message_get_name(sk_message_arg_at(msg, i)));
+    }
+    sk_method_set_argument_names(cloned, argument_names);
+    sk_method_set_message(cloned, sk_message_arg_at(msg, i));
     /* TODO: argument names */
     return cloned;
 }
@@ -42,7 +48,21 @@ SkObject *sk_method_call(SkObject *self, SkObject *parentctx, SkObject *call_msg
     SkObject *call = sk_object_clone(sk_vm_get_proto(SK_VM, "Call"));
     sk_call_set_message(call, call_msg);
     sk_object_set_slot(ctx, "call", call);
-
+    SkObject *argument_names = sk_method_get_argument_names(self);
+    ArgCount i, argnamecount = sk_list_size(argument_names);
+    ArgCount call_argcount = sk_list_size(sk_message_get_arguments(call_msg));
+    for(i = 0; i < argnamecount; i++) {
+        if(i < call_argcount) {
+            sk_object_set_slot(ctx,
+                    bstr2cstr(sk_string_get_bstring(sk_list_get_at(argument_names, i)), '\\'),
+                    sk_message_eval_arg_at(call_msg, i));
+        }
+        else {
+            sk_object_set_slot(ctx, 
+                    bstr2cstr(sk_string_get_bstring(sk_list_get_at(argument_names, i)), '\\'),
+                    SK_NIL);
+        }
+    }
     sk_vm_callstack_push(SK_VM, ctx);
     SkObject *result = sk_message_dispatch_avalanche(msg);
     sk_vm_callstack_pop(SK_VM);
--- src/method.h
+++ src/method.h
@@ -12,7 +12,8 @@ SkObject *sk_method_call(SkObject *self, SkObject *parentctx, SkObject *msg);
 
 SkObject *sk_method__clone(SkObject *slot, SkObject *self, SkObject *msg);
 
-#define sk_method_get_message(SELF) sk_object_get_slot_lazy(SELF, "message")
+#define sk_method_get_message(SELF) sk_object_get_slot_recursive(SELF, "message")
 #define sk_method_set_message(SELF, VALUE) sk_object_set_slot(SELF, "message", VALUE)
-
+#define sk_method_get_argument_names(SELF) sk_object_get_slot_recursive(SELF, "argument_names")
+#define sk_method_set_argument_names(SELF, VALUE) sk_object_set_slot(SELF, "argument_names", VALUE)
 #endif
