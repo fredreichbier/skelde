4f4ed030d5804b07f3506ae57e70bc8fcdfe73b8
Friedrich Weber <fred@reichbier.de>
2009-05-04 18:51:37 +0200

    uhuu, magic! (and `List set_at`)

--- src/list.c
+++ src/list.c
@@ -25,6 +25,7 @@ SkObject *sk_list_create_proto(SkVM *vm) {
     sk_object_set_clone_func(self, &sk_list_clone);
     /* methods */
     sk_object_bind_method(self, "get_at", &sk_list__get_at);
+    sk_object_bind_method(self, "set_at", &sk_list__set_at);
     sk_object_bind_method(self, "append", &sk_list__append);
     return self;
 }
@@ -45,6 +46,14 @@ SkObject *sk_list_get_at(SkObject *self, int index) {
     return item;
 }
 
+void sk_list_set_at(SkObject *self, int index, SkObject *value) {
+    if(cvector_set_element(sk_list_get_data(self), &value, index) != 0) {
+        sk_exc_raise(SK_VM, sk_exception_create_lazy(SK_VM,
+                    "IndexError",
+                    bformat("Couldn't access index %d.", index)));
+    }
+}
+
 SkObject *sk_list_create(SkVM *vm) {
     SkObject *self = sk_object_clone(sk_vm_get_proto(vm, "List"));
     return self;
@@ -59,6 +68,13 @@ SkObject *sk_list__get_at(SkObject *slot, SkObject *self, SkObject *msg) {
 SkObject *sk_list__append(SkObject *slot, SkObject *self, SkObject *msg) {
     sk_message_check_argcount(msg, "List append", 1);
     sk_list_append(self, sk_message_eval_arg_at(msg, 0));
+    return self;
+}
+
+SkObject *sk_list__set_at(SkObject *slot, SkObject *self, SkObject *msg) {
+    sk_message_check_argcount(msg, "List set_at", 1);
+    sk_list_set_at(self, sk_number_get_int(sk_message_eval_arg_at(msg, 0)),
+            sk_message_eval_arg_at(msg, 1));
     return self;
 }
 
--- src/list.h
+++ src/list.h
@@ -9,10 +9,12 @@ SkObject *sk_list_clone(SkObject *self);
 SkObject *sk_list_create_proto(SkVM *vm);
 void sk_list_append(SkObject *self, SkObject *item);
 SkObject *sk_list_get_at(SkObject *self, int index);
+void sk_list_set_at(SkObject *self, int index, SkObject *value);
 SkObject *sk_list_create(SkVM *vm);
 
 SkObject *sk_list__get_at(SkObject *slot, SkObject *self, SkObject *msg);
 SkObject *sk_list__append(SkObject *slot, SkObject *self, SkObject *msg);
+SkObject *sk_list__set_at(SkObject *slot, SkObject *self, SkObject *msg);
 
 #define sk_list_get_data(obj) \
     ((CVector *)sk_object_get_data(obj))
