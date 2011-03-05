8871475e28611d85c5147b8ced51b4368c2790c7
Friedrich Weber <fred@reichbier.de>
2011-03-05 17:47:29 +0100

    Huh, I wasn't even using the GC. Stupid me. Reenabled it. Should also work with threads.

--- src/mem.h
+++ src/mem.h
@@ -5,23 +5,31 @@
 #include <memory.h>
 #include <stdlib.h>
 
+#include <gc.h>
+void * GC_dlopen(const char *path, int mode);
+
+#define GC_PTHREADS
+#include <gc/gc_pthread_redirects.h>
 #include <pthread.h>
 
 static inline void *sk_malloc(size_t size) {
-    return malloc(size);
+    return GC_MALLOC(size);
 }
 
 static inline void *sk_realloc(void *ptr, size_t size) {
-    return realloc(ptr, size);
+    return GC_realloc(ptr, size);
 }
 
 static inline void *sk_calloc (size_t nelem, size_t size) {
-  return calloc(nelem, size);
+   void* p = sk_malloc (nelem * size);
+   assert(p != NULL);
+   memset (p, 0, nelem * size);
+   return p;
 }
 
 static inline void sk_free(void *ptr) {
     // that's a no-op here.
-    free(ptr);
+    GC_FREE(ptr);
 }
 
 #endif
